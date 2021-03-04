993fb8a22ac44fe87dc6c3f4626db2940a9b1c1b
Akisole <drive_98@mail.ru>
2021-03-05 00:41:15 +0300

    fix: implement subtasks 'Count cats', 'Dream team'

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  let numCats = 0;
+
+  matrix.forEach(element => {
+    element.forEach(find => {
+      if (find == '^^') numCats++;
+    });
+  });
+
+  return numCats;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(members)) return false;
+
+  let team = "";
+
+  members.forEach(element => {
+    if (isString(element)){
+      let tmp = element.split(" ").join("");
+      team += tmp[0].toUpperCase();
+    }      
+  });
+
+  team = team.split("").sort().join("");
+
+  return team;
 };
+
+function isString(val) {
+  return (typeof val === "string" || val instanceof String);
+}
